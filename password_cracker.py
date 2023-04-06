798f800d07b2d7bd2bdc03b29c44b06e11214991
L41T <43862672+CGM33@users.noreply.github.com>
2023-04-07 00:04:38 +0200

    Add files via upload

---
+++ password_cracker.py
@@ -0,0 +1,42 @@
+from hashlib import sha1
+
+def hashPass(arr):
+    newArr = []
+    for a in arr:
+        newArr.append(sha1(a.encode()).hexdigest())
+    return newArr
+
+def getPwd(pwdList, hashList, hash):
+    try:
+        index = hashList.index(hash)
+        return pwdList[index]
+    except ValueError:
+        return "PASSWORD NOT IN DATABASE"
+
+def crack_sha1_hash(hash, use_salts=False):
+
+    # Load the text files
+    with open("top-10000-passwords.txt", encoding='utf-8') as f:
+        passwords = f.read().splitlines()
+
+    with open("known-salts.txt", encoding='utf-8') as f:
+        salts = f.read().splitlines()
+
+    if use_salts:
+        passWithSalt = []
+        newPasswords = []
+        for s in salts:
+            for p in passwords:
+                #https://forum.freecodecamp.org/t/fcc-brute-force-password-cracker-hint-to-salted/418204/3
+                passWithSalt.append(f'{p}{s}')
+                passWithSalt.append(f'{s}{p}')
+                newPasswords.append(p)
+                newPasswords.append(p)
+                # NOT THIS:
+                #passWithSalt.append(f'{s}{p}{s}')
+
+        hashedPass = hashPass(passWithSalt)
+        return getPwd(newPasswords, hashedPass, hash)
+    else:
+        hashedPass = hashPass(passwords)
+        return getPwd(passwords, hashedPass, hash)
